687a95f75b8499255ff2b11ffd276eba6ddead73
Hayden <leahmelanie999@gmail.com>
2023-04-23 13:48:37 -0400

    Working the bullet

---
+++ Space Invaders/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public float xSpace;
+    public float xOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            Instantiate(enemyPrefab, new Vector2(x * xSpace + xOffset, 3), Quaternion.identity);
+            Instantiate(enemyPrefab, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(enemyPrefab, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
+        }
+
+    }
+}

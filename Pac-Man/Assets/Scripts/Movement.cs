46abce997f737df10c797dd143d01d6ad140a0b9
Aroosh <markhamonca@codeninjas.com>
2023-06-04 13:56:54 -0400

    Step 58
    Step 58

--- Pac-Man/Assets/Scripts/Movement.cs
+++ Pac-Man/Assets/Scripts/Movement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed;
+    public Vector2 intialDirection;
+    public LayerMask obstacleLayer;
+
+    protected Rigidbody2D rb;
+    protected Vector2 direction;
+    protected Vector2 nextDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        direction = intialDirection;
+        nextDirection = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+
     }
 }
---
+++ Pac-Man/Assets/Scripts/Pacman.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacman : Movement
+{
+     private bool Occupied(Vectir2 newDirection)
+    {
+
+    }
+}
